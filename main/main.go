c4dbba5f3ce4e1334d019b4d03a47bc26933f59b
barton1030 <barton0803@163.com>
2023-01-14 17:01:55 +0800

    添加配置解析文件、路由基础文件、main入口执行逻辑

--- main/main.go
+++ main/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"context"
+	"github.com/fvbock/endless"
+	"handshake/conf"
+	"handshake/router"
+	"strconv"
+)
+
+func main() {
+	conf.Init()
+	context.Background()
+	r := router.Router()
+	addr := structure()
+	endless.ListenAndServe(addr, r)
+}
+
+func structure() (addr string) {
+	server := conf.ServerConf()
+	addr = ":"
+	portString := strconv.Itoa(server.ServerPort)
+	addr = addr + portString
+	return addr
+}
--- router/router.go
+++ router/router.go
@@ -1,1 +1,13 @@
 package router
+
+import "github.com/gin-gonic/gin"
+
+func Router() *gin.Engine {
+	root := gin.Default()
+	user(root)
+	return root
+}
+
+func user(r *gin.Engine) {
+
+}
