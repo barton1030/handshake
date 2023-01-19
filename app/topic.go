06934fa52c2de0e9743f1dacdb2dabd4f4aa625a
barton1030 <barton0803@163.com>
2023-01-19 13:23:05 +0800

    在topic模块中添加http请求模版方法、路由等

--- app/topic.go
+++ app/topic.go
@@ -1,1 +1,23 @@
 package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"handshake/helper"
+)
+
+type topic struct {
+}
+
+var TopicController topic
+
+func (t topic) Add(c *gin.Context) {
+	helper.Response(c, 0, nil, "")
+}
+
+func (t topic) Edit(c *gin.Context) {
+
+}
+
+func (t topic) Delete(c *gin.Context) {
+
+}
--- router/router.go
+++ router/router.go
@@ -1,13 +1,22 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"handshake/app"
+)
 
 func Router() *gin.Engine {
 	root := gin.Default()
 	user(root)
+	topic(root)
 	return root
 }
 
 func user(r *gin.Engine) {
 
+}
+
+func topic(r *gin.Engine) {
+	topicGroup := r.Group("/topic")
+	topicGroup.POST("/add", app.TopicController.Add)
 }
