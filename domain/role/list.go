32f8ce69466ab9862b80eb9a4b8ac92fe575c847
barton1030 <barton0803@163.com>
2023-01-31 20:10:45 +0800

    领域模块填充角色、角色列表对象

--- domain/role/list.go
+++ domain/role/list.go
@@ -1,1 +1,25 @@
 package role
+
+type list struct {
+	storage map[int]role
+}
+
+func (l *list) Add() (err error) {
+	return err
+}
+
+func (l *list) Edit(cRole role) (err error) {
+	return err
+}
+
+func (l *list) Delete(roleId int) (err error) {
+	return err
+}
+
+func (l *list) List() (roleList map[int]role, err error) {
+	return
+}
+
+func (l *list) Role(roleId int) (cRole role, err error) {
+	return
+}
--- domain/role/role.go
+++ domain/role/role.go
@@ -1,1 +1,29 @@
 package role
+
+import (
+	"sync"
+	"time"
+)
+
+type role struct {
+	id             int
+	name           string
+	permissionMap  map[string]bool
+	permissionLock sync.Mutex
+	creator        int
+	createTime     time.Time
+}
+
+func NewRole(name string, creator int) role {
+	return role{
+		name:       name,
+		creator:    creator,
+		createTime: time.Now(),
+	}
+}
+
+func (r *role) SetPermission(permissionKey string, permissionValue bool) {
+	r.permissionLock.Lock()
+	defer r.permissionLock.Unlock()
+	r.permissionMap[permissionKey] = permissionValue
+}
