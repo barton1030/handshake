03788a05a9d215ef50952286f87ad6988986e4c8
barton1030 <barton0803@163.com>
2023-01-31 10:15:16 +0800

    为topic接口添加回调、告警、消息队列、消息接口

--- Interface/topic.go
+++ Interface/topic.go
@@ -5,4 +5,30 @@ type Topic interface {
 	MinConcurrency() int
 	MaxConcurrency() int
 	FuseSalt() int
+	CallbackHandler() Callback
+	AlarmHandler() Alarm
+	MessageQueuingHandler() MessageQueuing
+}
+
+type Callback interface {
+	Do() (res map[string]interface{}, err error)
+}
+
+type Alarm interface {
+	Do(information string, recipients []interface{})
+}
+
+type MessageQueuing interface {
+	Pop() (message Message, err error)
+	Push(message Message) (err error)
+	Count() (count int)
+}
+
+type Message interface {
+	Data() (data map[string]interface{}, err error)
+	RetryCount() (retryCont int)
+	IncrRetryCont()
+	Processable() (processable bool)
+	Success()
+	Fail()
 }
